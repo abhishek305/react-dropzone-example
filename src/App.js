fadb3345d8ce1897eec59d94344174525ceb21b6
Abhishek Ezhava <abhishek.ezhava@contentstack.com>
2023-09-13 18:03:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import UploadFile from "./components/UploadFile";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Drop file below </h1>
+      <UploadFile />
+    </div>
+  );
+}
+
+export default App;
